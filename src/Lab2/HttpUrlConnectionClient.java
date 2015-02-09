9a8e3e2962ed205beb8cedf0606770d3ea9e4eaf
fika4life <fistewie@gmail.com>
2015-02-09 17:20:36 +0100

    latest version

--- src/Lab2/HttpUrlConnectionClient.java
+++ src/Lab2/HttpUrlConnectionClient.java
@@ -25,45 +25,61 @@ public class HttpUrlConnectionClient {
 
 
 
+            int rounds = 100;
+            int[] guesses = new int[rounds];
 
             int guess=50;
             String clientId=null;
-            while(true){
-                url = new URL("http", IP, 8080, "/?guess=" + guess);
-                HttpURLConnection con = null;
-                con = (HttpURLConnection) url.openConnection();
 
-                con.setRequestProperty("User-Agent", "Mozilla");
+            for(int i = 0 ; i<rounds; i++) {
+                int guessNumber = 0;
 
-                if (clientId !=null){
-                    con.setRequestProperty("Cookie", "clientId=" + clientId);
+                while (true) {
+                    guessNumber++;
+                    url = new URL("http", IP, 8080, "/?guess=" + guess);
+                    HttpURLConnection con = null;
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestProperty("User-Agent", "Mozilla");
+
+                    if (clientId != null) {
+                        con.setRequestProperty("Cookie", "clientId=" + clientId);
+                    }
+
+                    con.connect();
+
+                    String response = convertStreamToString(con.getInputStream());
+
+
+                    String clientIdRaw = con.getHeaderField("Set-Cookie");
+                    if (clientIdRaw != null) {
+                        clientId = clientIdRaw.replace("clientId=", "");
+                    }
+
+                    if (response.contains("CORRECT!")) {
+                        guesses[i] = guessNumber;
+                        break;
+                    } else {
+                        Pattern pattern = Pattern.compile("<p>The number is between: ([0-9]+) and ([0-9]+)</p>");
+                        Matcher matcher = pattern.matcher(response);
+                        matcher.find();
+                        int lowerInterval = Integer.parseInt(matcher.group(1));
+                        int higherInterval = Integer.parseInt(matcher.group(2));
+                        //System.out.println(lowerInterval + " " + higherInterval);
+                        guess = (int) Math.floor((lowerInterval + higherInterval) / 2);
+
+                    }
+
+
                 }
-
-                con.connect();
-
-                String response = convertStreamToString(con.getInputStream());
-
-
-                String clientIdRaw = con.getHeaderField("Set-Cookie");
-                if (clientIdRaw != null){
-                    clientId = clientIdRaw.replace("clientId=", "");
-                }
-
-                if(response.contains("CORRECT!")){
-                    break;
-                }else{
-                    Pattern pattern = Pattern.compile("<p>The number is between: ([0-9]+) and ([0-9]+)</p>");
-                    Matcher matcher = pattern.matcher(response);
-                    matcher.find();
-                    int lowerInterval = Integer.parseInt(matcher.group(1));
-                    int higherInterval = Integer.parseInt(matcher.group(2));
-                    System.out.println(lowerInterval + " " + higherInterval);
-                    guess = (int)Math.floor((lowerInterval + higherInterval)/2);
-                }
-
+            }
+            int totalGuesses = 0;
+            for (int theGuess:guesses){
+                totalGuesses+=theGuess;
 
             }
-
+            double averageGuesses = (double) totalGuesses/(double)rounds;
+            System.out.println(averageGuesses);
         }
         catch(IOException e){
             e.printStackTrace();
@@ -78,4 +94,3 @@ public class HttpUrlConnectionClient {
 
 }
 
-///Kör 100 gångar. 
