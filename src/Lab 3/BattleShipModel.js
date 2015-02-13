511d28c2f36c3782a434e23ad1a5b891f9edc556
Fredrik <fredde_wallen@hotmail.com>
2015-02-13 14:55:47 +0100

    Finished lab 3

--- src/Lab 3/BattleShipModel.js
+++ src/Lab 3/BattleShipModel.js
@@ -12,19 +12,16 @@ var BattleShipModel = function(rows,columns){
     this._shipLengths = [2,3,3,4,5];
     this._directions = [[1,0],[0,1]];
 
-    for (var i = 0; i < this._rows; i++) {
-        this._grid[i] = new Array(this._columns);
-        for(var j=0;j<this._columns;j++){
-            this._grid[i][j] = new BattleshipSquare();
-        }
-    }
+
+
 
     this._randomNumber=function(min,max){
         return (Math.floor(Math.random() * (max - min + 1)) + min);
     };
 
     this._populate=function(){
-        for (var shipLength in this._shipLengths){
+        for (var index in this._shipLengths){
+            shipLength=this._shipLengths[index];
             var canBePlaced=false;
             while(!canBePlaced) {
                 canBePlaced = true;
@@ -50,6 +47,20 @@ var BattleShipModel = function(rows,columns){
                 xChecked = xCoord + xDirection * i;
                 yChecked = yCoord + yDirection * i;
                 this._grid[xChecked][yChecked].addShip();
+            }
+        }
+    };
+
+    this.reset=function(){
+        this._createGrid();
+        this._populate();
+    }
+
+    this._createGrid=function(){
+        for (var i = 0; i < this._rows; i++) {
+            this._grid[i] = new Array(this._columns);
+            for(var j=0;j<this._columns;j++){
+                this._grid[i][j] = new BattleshipSquare();
             }
         }
     };
@@ -81,13 +92,24 @@ var BattleShipModel = function(rows,columns){
         if(yCoord>this._columns-1){
             throw  "Y-coordinate out of bounds: "+yCoord;
         }
-        return this._grid[xCoord,yCoord].getAttacked();
+        return this._grid[xCoord][yCoord].getAttacked();
+    };
+
+    this.checkIfVictory=function(){
+        for(var i=0;i<rows;i++){
+            for(var j=0;j<columns;j++){
+                var square=this._grid[i][j];
+                if((!square.getAttacked())&&square.getHasShip()){
+                    return false;
+                }
+            }
+        }
+        return true;
     };
 
 
-
+    this._createGrid();
     this._populate();
-    console.log(this.checkHasShip(2,6));
 
 
 
--- src/Lab 3/Battleship.js
+++ src/Lab 3/Battleship.js
@@ -1,43 +1,18 @@
-var rows=9;
-var columns=9;
-
-var model=new BattleShipModel(rows,columns);
 
 
-function showStatus(){
-    for(var i=0;i<rows;i++){
-        for(j=0;j<columns;j++){
-            if(model.checkAttacked(i,j)){
-                var square=$("#"+i+"x"+j);
-                if(model.checkHasShip(i,j)) {
-                    if (!square.hasClass("attacked")) {
-                        square.addClass("attacked");
-                    }
-                }
-                else {
-                    if (!square.hasClass("missed")) {
-                        square.addClass("missed");
-                    }
-                }
-            }
-        }
-    }
-}
+
+
+
+
+
+
 
 $(function() {
+    var rows=9;
+    var columns=9;
 
-    for(var i=0;i<rows;i++){
-        var row='<div class="row">';
-        for(j=0;j<columns;j++){
-            row+='<div class="square"; id="'+i+"x"+j+'" />';
-        }
-        row+='</div>';
-        $("#board").append(row);
-        $(".square").click(function(event){
-            var idString=event.target.id;
-            $("#"+idString).css("background-color", "#00FF00");
-            $("#text-info").html(idString);
-        });
-    }
+    var model=new BattleShipModel(rows,columns);
+    var view=new BattleshipView($('#board'),model,rows,columns);
+    var controller=new BattleshipController(view,model);
 });
 
